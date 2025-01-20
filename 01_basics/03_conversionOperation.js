5f00e084cb382b7aeb57ca5e659a9f76d1c1683a
Niva-awale1 <117263124+Niva-awale1@users.noreply.github.com>
2025-01-20 07:54:31 +0000

    conversions of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = undefined
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+//console.log(valueInNumber )
+
+// "50" => 50
+// "33abc" => NaN
+// true => 1 ; false => 0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = "Niva"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 50
+
+let stringNumber = String(someNumber)
+
+console.log( typeof stringNumber);
+
+
