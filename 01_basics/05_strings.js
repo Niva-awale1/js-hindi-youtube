64d8219fdc3ef2e026d27f654c445d4c60bf5987
Niva-awale1 <117263124+Niva-awale1@users.noreply.github.com>
2025-01-21 04:02:23 +0000

    comparision

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,4 @@
+const name = "Niva"
+const repoCount = 50
+
+console.log(name);
---
+++ datatype-summary.js
@@ -0,0 +1,11 @@
+const score = 500
+const scoreValue = 501.4
+const isLoggedIn =  false
+const outsideTemp = null
+
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)
