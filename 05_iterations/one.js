30f3ca66155834a886953b9623d08cc36d70cc16
Niva-awale1 <117263124+Niva-awale1@users.noreply.github.com>
2025-01-24 05:38:25 +0000

    week5

---
+++ 05_iterations/one.js
@@ -0,0 +1,22 @@
+//for loop
+
+for (let i = 0; i <= 20; i++) {
+    const element = i;
+    if (element == 5) {
+        // console.log("5 is the best number");
+        
+    }
+
+    // console.log(element);
+    
+}
+
+let myArray = ["rohan", "nirohan", "niva", "aiden", "nishchal", "aditi", "sasita"]
+console.log(myArray.length);
+
+for (let i = 0; i < myArray.length; i++) {
+    const element = myArray[i];
+    console.log(element);
+    
+}
+
---
+++ 05_iterations/two.js
@@ -0,0 +1,15 @@
+
+// let index = 0
+// while (index <= 20) {
+//     console.log(`value of index is ${index}`);
+//     index = index + 2
+// }
+
+let myArray = ["Niva", "rohan", "Maharjan", "Awale"]
+
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`Value is ${myArray [arr]}`);
+    arr = arr + 1
+    
+}
